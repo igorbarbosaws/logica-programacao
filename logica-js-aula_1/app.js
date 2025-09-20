9768352b09e46b3ea961d7a1fd722cba1f2e0cc0
Igor Barbosa <igorbarbosaws@gmail.com>
2025-09-20 00:22:15 -0300

    Update app.js

--- logica-js-aula_1/app.js
+++ logica-js-aula_1/app.js
@@ -1,14 +1,19 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 9;
 console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute
 
-//se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else{
-    if(chute > numeroSecreto)
+//enquanto o chute não for igual ao número secreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    //se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+    } else{
+        if(chute > numeroSecreto{
             alert(`o número é menor que o ${chute}`);
-    } else
-        alert(`o número é maior que o ${chute}`);
+        } else {
+         alert(`o número é maior que o ${chute}`)
+        }
+    }
 }
