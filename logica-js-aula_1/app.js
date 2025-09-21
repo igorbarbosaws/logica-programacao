6b95a50e84ee3e44f133c93d77c3f31210a3eb79
Igor Barbosa <igorbarbosaws@gmail.com>
2025-09-21 10:54:10 -0300

    Update app.js
    Adicionei o while no código e um contador de tentativas até o acerto.

--- logica-js-aula_1/app.js
+++ logica-js-aula_1/app.js
@@ -1,19 +1,22 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 9;
-console.log(numeroSecreto)
-let chute
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
 //enquanto o chute não for igual ao número secreto
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
     //se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-    } else{
-        if(chute > numeroSecreto{
-            alert(`o número é menor que o ${chute}`);
+            alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+}    else{
+        if(chute > numeroSecreto) {
+            alert(`O número é menor que o ${chute}`);
         } else {
-         alert(`o número é maior que o ${chute}`)
-        }
+            alert(`O número é maior que o ${chute}`);
+     }
+     //tentativas = tentativas + 1;
+     tentativas++
     }
 }
